78603cf54613973e48ed5dec1f4d05bbe183d1e2
DaffPunks <sawavas2@gmail.com>
2016-12-07 23:41:59 +0700

    (u) updated screens

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,17 @@
 $(function () {
     let is_hidden = false;
 
-
-    $(".i-open-button").click(function () {
-        $(".desktop-ide").removeClass("desktop-ide-anim");
-        $(".desktop-bar-ide").removeClass("desktop-bar-anim");
-        $(".ide-text").removeClass("ide-text-anim");
-        $(".i-open-button").removeClass("i-open-button-anim");
-        is_hidden = false;
-    });
-
     $(".i-close-button").click(function () {
         $(".desktop-ide").addClass("desktop-ide-anim");
         $(".desktop-bar-ide").addClass("desktop-bar-anim");
         $(".ide-text").addClass("ide-text-anim");
         $(".i-open-button").addClass("i-open-button-anim");
+        is_hidden = true;
+    });
+
+    $(".br-close-button").click(function () {
+        $(".desktop-browser").addClass("desktop-br-anim");
+        $(".desktop-bar-br").addClass("desktop-bar-anim");
         is_hidden = true;
     });
 
@@ -26,13 +23,21 @@ $(function () {
         }
     });
 
+    $(".br-fullscreen-button").click(function () {
+        if($(".desktop-browser").hasClass("desktop-br-fullscreen")) {
+            $(".desktop-browser").removeClass("desktop-br-fullscreen");
+        } else {
+            $(".desktop-browser").addClass("desktop-br-fullscreen");
+        }
+    });
+
     $(".icon-wrapper").hover(function () {
        $(".dock").addClass("dock-hovered");
     }, function () {
         $(".dock").removeClass("dock-hovered");
     });
 
-    $(".backend-icon").click(function () {
+    $(".icon-wrapper").has(".backend-icon").click(function () {
         if($(".desktop-ide").hasClass("desktop-ide-anim")) {
             $(".desktop-ide").removeClass("desktop-ide-anim");
             $(".desktop-bar-ide").removeClass("desktop-bar-anim");
@@ -46,4 +51,34 @@ $(function () {
         }
     });
 
+    $(".icon-wrapper").has(".frontend-icon").click(function () {
+        if($(".desktop-browser").hasClass("desktop-br-anim")) {
+            $(".desktop-browser").removeClass("desktop-br-anim");
+            $(".desktop-bar-br").removeClass("desktop-bar-anim");
+        } else {
+            $(".desktop-browser").addClass("desktop-br-anim");
+            $(".desktop-bar-br").addClass("desktop-bar-anim");
+        }
+    });
+
 });
+$PHP = 1;
+$database = 1;
+
+function backend() {
+    if($PHP) {
+        // Laravel 5
+    }
+    if($database) {
+        // MySQL, PostgreSQL, FireBird, Oracle
+    }
+
+    $other = [
+        'RESTful API',
+        'OOP',
+        'Design Patterns',
+        'Data Structure and Algorithms'
+    ];
+}
+
+backend();
