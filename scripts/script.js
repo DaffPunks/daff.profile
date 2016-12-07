06f64232c7946e7a682880e75774fd3be441ba2b
DaffPunks <sawavas2@gmail.com>
2016-12-07 18:32:37 +0700

    (+) added fullscreen and functionality for dockbar

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,32 @@ $(function () {
         is_hidden = true;
     });
 
+    $(".i-fullscreen-button").click(function () {
+        if($(".desktop-ide").hasClass("desktop-ide-fullscreen")) {
+            $(".desktop-ide").removeClass("desktop-ide-fullscreen");
+        } else {
+            $(".desktop-ide").addClass("desktop-ide-fullscreen");
+        }
+    });
+
     $(".dock-icon").hover(function () {
        $(".dock").addClass("dock-hovered");
     }, function () {
         $(".dock").removeClass("dock-hovered");
+    });
+
+    $(".backend-icon").click(function () {
+        if($(".desktop-ide").hasClass("desktop-ide-anim")) {
+            $(".desktop-ide").removeClass("desktop-ide-anim");
+            $(".desktop-bar-ide").removeClass("desktop-bar-anim");
+            $(".ide-text").removeClass("ide-text-anim");
+            $(".i-open-button").removeClass("i-open-button-anim");
+        } else {
+            $(".desktop-ide").addClass("desktop-ide-anim");
+            $(".desktop-bar-ide").addClass("desktop-bar-anim");
+            $(".ide-text").addClass("ide-text-anim");
+            $(".i-open-button").addClass("i-open-button-anim");
+        }
     });
 
 });
