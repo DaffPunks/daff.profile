a82109112ad58ff6e4c2de6edd861aaee87a106d
DaffPunks <sawavas2@gmail.com>
2016-12-07 11:00:08 +0700

    (I) Initial

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    let is_hidden = false;
+
+
+    $(".i-open-button").click(function () {
+        $(".desktop-ide").removeClass("desktop-ide-anim");
+        $(".desktop-bar-ide").removeClass("desktop-bar-anim");
+        $(".ide-text").removeClass("ide-text-anim");
+        $(".i-open-button").removeClass("i-open-button-anim");
+        is_hidden = false;
+    });
+
+    $(".i-close-button").click(function () {
+        $(".desktop-ide").addClass("desktop-ide-anim");
+        $(".desktop-bar-ide").addClass("desktop-bar-anim");
+        $(".ide-text").addClass("ide-text-anim");
+        $(".i-open-button").addClass("i-open-button-anim");
+        is_hidden = true;
+    });
+
+    $(".dock-icon").hover(function () {
+       $(".dock").addClass("dock-hovered");
+    }, function () {
+        $(".dock").removeClass("dock-hovered");
+    });
+
+});
